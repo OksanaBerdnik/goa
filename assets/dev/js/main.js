0c9caaf4932c2c9c47e5702a616c4ec4dff62fb2
Oksana_Berdnik <oksana.berdnik@masterofcode.com>
2017-11-06 02:10:41 +0200

    updated masters

--- assets/dev/js/main.js
+++ assets/dev/js/main.js
@@ -113,14 +113,3 @@ function myMap(loc1, loc2, notice) {
     });
     infowindow.open(map,marker);
 }
-
-
-(function(){
-    var widget_id = 'cXdlneDcoU';
-    var d=document;var w=window;function l(){
-    var s = document.createElement('script');
-    s.type = 'text/javascript'; s.async = true;
-    s.src = '//code.jivosite.com/script/widget/'+widget_id;
-    var ss = document.getElementsByTagName('script')[0];
-    ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();
-    }else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
